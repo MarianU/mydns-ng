9cca30753df2fd8a5c8c241ad13af7771134d200
Marian-Daniel Ursache <ursache_marian@yahoo.co.uk>
2024-12-16 16:33:17 +0200

    Update alpine and tools in container

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -f docker/Dockerfile --tag docker.io/hostingpro/mydns-ng:v0.0.2-1.2.8.31
+podman build . -f docker/Dockerfile --tag docker.io/hostingpro/mydns-ng:v0.0.2-1.2.8.31
 
